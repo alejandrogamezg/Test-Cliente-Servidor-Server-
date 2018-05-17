998218350349c6e3c4e565f49adfdc060a3bb7e1
Alejandro Gamez <alejandrogamezg@gmail.com>
2018-05-17 14:48:08 -0500

    Test Cliente-Servidor(Server)
    v1.0
    esta es la primara version de le proyecto cliente-servidor

---
+++ Assets/ServerClient_Setup/Scripts/Example/EnableDisable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class EnableDisable : NetworkBehaviour
+{
+
+
+	[ClientRpc]
+	void RpcEnableDisable()
+	{
+
+		Test();
+		
+		
+	}
+	void Test()
+	{
+		Debug.Log("HOLA");
+	}
+
+	public void BtnEnableDisable()
+	{
+		 if(isServer)
+		RpcEnableDisable();
+	}
+}
