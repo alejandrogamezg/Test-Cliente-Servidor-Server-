08e77f4f5590af05f69526c86d2c858f35a04fc7
Alejandro Gamez <alejandrogamezg@gmail.com>
2018-05-17 19:55:57 -0500

    activa desactiva servidor
    se establecio la funcion en el servidor para mandar activar o desactivar un objeto en el cliente

--- Assets/ServerClient_Setup/Scripts/Example/EnableDisable.cs
+++ Assets/ServerClient_Setup/Scripts/Example/EnableDisable.cs
@@ -6,23 +6,49 @@ using UnityEngine.UI;
 public class EnableDisable : NetworkBehaviour
 {
 
+	public GameObject cubo;
+	public bool enaDisa = false;
+
+
+	[ClientRpc]
+	void RpcTest()
+	{
+		Test();	
+	}
 
 	[ClientRpc]
 	void RpcEnableDisable()
 	{
-
-		Test();
-		
-		
+		ActivaDesactiva();	
 	}
+
 	void Test()
 	{
 		Debug.Log("HOLA");
 	}
 
+	void ActivaDesactiva(){
+		enaDisa = !enaDisa;
+		if(enaDisa == true)
+		{
+			cubo.SetActive(true);
+		}
+		if(enaDisa == false)
+		{
+			cubo.SetActive(false);
+		}
+	}
+
+	public void BtnTest()
+	{
+		 if(isServer)
+		Test();
+		RpcTest();
+	}
 	public void BtnEnableDisable()
 	{
 		 if(isServer)
+		 ActivaDesactiva();
 		RpcEnableDisable();
 	}
 }
